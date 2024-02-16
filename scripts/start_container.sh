a4ea93b4423ede7ccdff499e51a4eeec8903c9c4
Priyadarshi ranjan <119883079+priyadarshi0811@users.noreply.github.com>
2024-02-16 16:40:01 +0530

    deploy code

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
+echo
  docker pull darshif5/simple-python-flask-app
 
 # Run the Docker image as a container
+echo
 docker run -d -p 5000:5000 darshif5/simple-python-flask-app
